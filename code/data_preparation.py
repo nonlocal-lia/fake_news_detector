aa4bf1421f0b17db9fa3c46d5bda9745840f86a1
Lia Elwonger <lelwonger@gmail.com>
2022-03-07 20:03:07 -0600

    Used LDA to do topic analysis and added to the clean data

--- code/data_preparation.py
+++ code/data_preparation.py
@@ -23,11 +23,11 @@ def get_scores(model, X_test, y_test):
     output = {'Accuracy': acc, "Precision": prec, "Recall": rec, "F1 Score": f1}
     return output
 
-def get_network_metrics(model, X_train, y_train, X_test, y_test, batch_size=32):
+def get_network_metrics(model, X_train, y_train, X_test, y_test, batch_size=32, rare_cat=False):
 
     """
     Gets accuracy, and weighted F1 scores of a Keras classification model for both training and test data
-    Also determines the rarest category in the data and calculates the model's precision, recall, and F1
+    If rare_cat = True, also determines the rarest category in the data and calculates the model's precision, recall, and F1
     on the rare category
 
     Arg:
@@ -58,17 +58,18 @@ def get_network_metrics(model, X_train, y_train, X_test, y_test, batch_size=32):
     print(f'Train Average Weighted F1 Score: {train_f1:.3}')
     train_cm = confusion_matrix(rounded_train_labels, rounded_train_predictions)
     length_train = len(rounded_train_labels)
-    for i in range(train_cm.shape[0]):
-        count = np.count_nonzero(rounded_train_labels == i)
-        if count < length_train:
-            length_train = count
-            index_value = i
-    recall_rare_train = train_cm[index_value, index_value]/np.sum(train_cm[0])
-    print(f'Train Recall on Rarest Category: {recall_rare_train:.3}')
-    precision_rare_train = train_cm[index_value, index_value]/np.sum(train_cm[:,0])
-    print(f'Train Precision on Rarest Category: {precision_rare_train:.3}')
-    f1_rare_train = 2*(recall_rare_train*precision_rare_train)/(recall_rare_train+precision_rare_train)
-    print(f'Train F1 on Rarest Category: {f1_rare_train:.3}')
+    if rare_cat:
+        for i in range(train_cm.shape[0]):
+            count = np.count_nonzero(rounded_train_labels == i)
+            if count < length_train:
+                length_train = count
+                index_value = i
+        recall_rare_train = train_cm[index_value, index_value]/np.sum(train_cm[0])
+        print(f'Train Recall on Rarest Category: {recall_rare_train:.3}')
+        precision_rare_train = train_cm[index_value, index_value]/np.sum(train_cm[:,0])
+        print(f'Train Precision on Rarest Category: {precision_rare_train:.3}')
+        f1_rare_train = 2*(recall_rare_train*precision_rare_train)/(recall_rare_train+precision_rare_train)
+        print(f'Train F1 on Rarest Category: {f1_rare_train:.3}')
 
     # Evaluating Testing
     results_test = model.evaluate(X_test, y_test)
@@ -78,27 +79,35 @@ def get_network_metrics(model, X_train, y_train, X_test, y_test, batch_size=32):
     print(f'Test Average Weighted F1 Score: {test_f1:.3}')
     test_cm = confusion_matrix(rounded_test_labels, rounded_test_predictions)
     length_test = len(rounded_test_labels)
-    for i in range(test_cm.shape[0]):
-        count = np.count_nonzero(rounded_test_labels == i)
-        if count < length_test:
-            length_test = count
-            index_value = i
-    recall_rare_test = test_cm[index_value, index_value]/np.sum(test_cm[0])
-    print(f'Test Recall on Rarest Category: {recall_rare_test:.3}')
-    precision_rare_test = test_cm[index_value, index_value]/np.sum(test_cm[:,0])
-    print(f'Test Precision on Rarest Category: {precision_rare_test:.3}')
-    f1_rare_test = 2*(recall_rare_test*precision_rare_test)/(recall_rare_test+precision_rare_test)
-    print(f'Test F1 on Rarest Category: {f1_rare_test:.3}')
+    if rare_cat:
+        for i in range(test_cm.shape[0]):
+            count = np.count_nonzero(rounded_test_labels == i)
+            if count < length_test:
+                length_test = count
+                index_value = i
+        recall_rare_test = test_cm[index_value, index_value]/np.sum(test_cm[0])
+        print(f'Test Recall on Rarest Category: {recall_rare_test:.3}')
+        precision_rare_test = test_cm[index_value, index_value]/np.sum(test_cm[:,0])
+        print(f'Test Precision on Rarest Category: {precision_rare_test:.3}')
+        f1_rare_test = 2*(recall_rare_test*precision_rare_test)/(recall_rare_test+precision_rare_test)
+        print(f'Test F1 on Rarest Category: {f1_rare_test:.3}')
 
-    output = {"Training Accuracy": results_train[1],
-            "Test Accuracy": results_test[1],
-            "Training F1": train_f1,
-            "Test F1": test_f1,
-            "Training Rare Recall": recall_rare_train,
-            "Test Rare Recall": recall_rare_test,
-            "Training Rare Precision": precision_rare_train,
-            "Test Rare Precision": precision_rare_test,
-            "Training Rare F1": f1_rare_train,
-            "Test Rare F1": f1_rare_test
-            }
+    if rare_cat:
+        output = {"Training Accuracy": results_train[1],
+        "Test Accuracy": results_test[1],
+        "Training F1": train_f1,
+        "Test F1": test_f1,
+        "Training Rare Recall": recall_rare_train,
+        "Test Rare Recall": recall_rare_test,
+        "Training Rare Precision": precision_rare_train,
+        "Test Rare Precision": precision_rare_test,
+        "Training Rare F1": f1_rare_train,
+        "Test Rare F1": f1_rare_test
+        }
+    else:
+        output = {"Training Accuracy": results_train[1],
+        "Test Accuracy": results_test[1],
+        "Training F1": train_f1,
+        "Test F1": test_f1
+        }
     return output
